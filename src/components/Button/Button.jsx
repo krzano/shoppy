e7658f345ef4935a95837835c9ea940d25269fff
Aleksander <atkrzanowski@gmail.com>
2023-07-21 11:53:02 +0200

    created Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,50 @@
 import { css, styled } from 'styled-components';
 
-const StyledButton = styled.button`
-	background-color: ${({ variant }) =>
-		variant === 'primary' ? 'red' : 'green'};
+const variantPrimaryStyles = css`
+	color: var(--color-neutral-0);
+	background-color: var(--color-primary-900);
+	transition: background-color 0.3s;
+	&:hover {
+		background-color: var(--color-primary-700);
+	}
+`;
+const variantSecondaryStyles = css`
+	color: var(--color-primary-900);
+	background-color: var(--color-neutral-0);
+	transition: background-color 0.3s;
+	&:hover {
+		background-color: var(--color-primary-100);
+	}
+`;
+const sizeSmallStyles = css`
+	font-size: 1.2rem;
+`;
+const sizeMediumStyles = css`
+	font-size: 1.6rem;
+`;
+const sizeBigStyles = css`
+	font-size: 2rem;
 `;
 
-const Button = ({ variant = 'primary', children }) => {
-	return <StyledButton variant={variant}>{children}</StyledButton>;
+const StyledButton = styled.button`
+	padding: 0.7em 2em;
+	font-weight: 500;
+	border-radius: var(--border-radius-pill);
+	border: 1px solid var(--color-primary-900);
+	cursor: pointer;
+	text-decoration: none;
+	${({ variant }) => variant === 'primary' && variantPrimaryStyles}
+	${({ variant }) => variant === 'secondary' && variantSecondaryStyles}
+	${({ size }) => size === 'small' && sizeSmallStyles}
+	${({ size }) => size === 'medium' && sizeMediumStyles}
+	${({ size }) => size === 'big' && sizeBigStyles}
+`;
+
+const Button = ({ as, to, variant = 'primary', size = 'medium', children }) => {
+	return (
+		<StyledButton as={as} to={to} variant={variant} size={size}>
+			{children}
+		</StyledButton>
+	);
 };
 export default Button;
--- src/core/Home/Home.jsx
+++ src/core/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 import StyledContentWrapper from '../../styles/StyledContentWrapper/StyledContentWrapper';
 import Button from '../../components/Button/Button';
+import { Link } from 'react-router-dom';
 const StyledHeader = styled.header`
 	padding: 6rem 4rem;
 	background-color: var(--color-neutral-0);
@@ -11,6 +12,9 @@ const StyledHeader = styled.header`
 	);
 	h1 {
 		color: var(--color-primary-900);
+		/* letter-spacing: 1px; */
+		/* line-height: 1.5; */
+		/* font-size: 5rem; */
 		text-transform: capitalize;
 		margin-bottom: 2rem;
 	}
@@ -20,8 +24,27 @@ const Home = () => {
 	return (
 		<>
 			<StyledHeader>
-				<h1>Home</h1>
+				<h1>
+					{/* Where Technology Meets Your Needs */}
+					The Ultimate Online Store for Tech Lovers
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+					asperiores error quibusdam.
+				</p>
+				<br />
+				<Button>Default</Button>
+				<Button variant='secondary' size='big'>
+					Secondary Big
+				</Button>
+				<Button as={Link} to='about'>
+					Link
+				</Button>
+				<Button variant='primary' size='small'>
+					Primary Small
+				</Button>
 			</StyledHeader>
+			<StyledContentWrapper></StyledContentWrapper>
 		</>
 	);
 };
