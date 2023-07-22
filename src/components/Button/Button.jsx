1210f27e7e960302bb4d55ce59ee99d71dd1e130
Aleksander <atkrzanowski@gmail.com>
2023-07-22 13:23:25 +0200

    Button.jsx fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,31 +1,5 @@
 import { css, styled } from 'styled-components';
 
-const variantPrimaryStyles = css`
-	color: var(--color-neutral-0);
-	background-color: var(--color-primary-900);
-	transition: background-color 0.3s;
-	&:hover {
-		background-color: var(--color-primary-700);
-	}
-`;
-const variantSecondaryStyles = css`
-	color: var(--color-primary-900);
-	background-color: var(--color-neutral-0);
-	transition: background-color 0.3s;
-	&:hover {
-		background-color: var(--color-primary-100);
-	}
-`;
-const sizeSmallStyles = css`
-	font-size: 1.2rem;
-`;
-const sizeMediumStyles = css`
-	font-size: 1.6rem;
-`;
-const sizeBigStyles = css`
-	font-size: 2rem;
-`;
-
 const StyledButton = styled.button`
 	display: inline-block;
 	padding: 0.7em 2em;
@@ -36,18 +10,57 @@ const StyledButton = styled.button`
 	border: 1px solid var(--color-primary-900);
 	cursor: pointer;
 	text-decoration: none;
-	${({ variant }) =>
-		(variant === 'primary' && variantPrimaryStyles) ||
-		(variant === 'secondary' && variantSecondaryStyles)}
-	${({ size }) =>
-		(size === 'small' && sizeSmallStyles) ||
-		(size === 'medium' && sizeMediumStyles) ||
-		(size === 'big' && sizeBigStyles)}
+	${({ size }) => {
+		if (size === 'small') {
+			return css`
+				font-size: 1.2rem;
+			`;
+		}
+		if (size === 'medium') {
+			return css`
+				font-size: 1.6rem;
+			`;
+		}
+		if (size === 'big') {
+			return css`
+				font-size: 2rem;
+			`;
+		}
+	}}
+	${({ variant }) => {
+		if (variant == 'primary') {
+			return css`
+				color: var(--color-neutral-0);
+				background-color: var(--color-primary-900);
+				transition: background-color 0.3s;
+				&:hover {
+					background-color: var(--color-primary-700);
+				}
+			`;
+		}
+		if (variant == 'secondary') {
+			return css`
+				color: var(--color-neutral-0);
+				background-color: var(--color-primary-900);
+				transition: background-color 0.3s;
+				&:hover {
+					background-color: var(--color-primary-700);
+				}
+			`;
+		}
+	}}
 `;
 
-const Button = ({ as, to, variant = 'primary', size = 'medium', children }) => {
+const Button = ({
+	as,
+	to,
+	variant = 'primary',
+	size = 'medium',
+	children,
+	...rest
+}) => {
 	return (
-		<StyledButton as={as} to={to} variant={variant} size={size}>
+		<StyledButton as={as} to={to} variant={variant} size={size} {...rest}>
 			{children}
 		</StyledButton>
 	);
