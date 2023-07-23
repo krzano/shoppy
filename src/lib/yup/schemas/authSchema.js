8b08d4239aba5de4b2395efb3aa31c8c29d40129
Aleksander <atkrzanowski@gmail.com>
2023-07-23 12:45:21 +0200

    chabged error messages

--- src/lib/yup/schemas/authSchema.js
+++ src/lib/yup/schemas/authSchema.js
@@ -3,16 +3,16 @@ import * as yup from 'yup';
 const loginSchema = yup.object().shape({
 	email: yup
 		.string()
-		.email('Please enter a valid email.')
+		.email('Please enter a valid email address.')
 		.required('Email is required.'),
 	password: yup
 		.string()
-		.min(6, `Password length must be at least 6 characters`)
-		.required('Required'),
+		.min(6, `Password length must be at least 6 characters.`)
+		.required('Please enter your password.'),
 	confirmPassword: yup
 		.string()
-		.oneOf([yup.ref('password'), null], 'Passwords do not match')
-		.required('Required'),
+		.oneOf([yup.ref('password'), null], 'Passwords do not match.')
+		.required('Please confirm your password.'),
 });
 
 export default loginSchema;
