21141531ba56d307a52445fd649940d9c5f4e3f2
Aleksander <atkrzanowski@gmail.com>
2023-07-24 22:30:07 +0200

    Increase padding in StyledSection

--- src/core/Home/Section/Section.jsx
+++ src/core/Home/Section/Section.jsx
@@ -2,13 +2,13 @@ import { styled } from 'styled-components';
 import StyledContentWrapper from '../../../styles/StyledContentWrapper/StyledContentWrapper';
 
 const StyledSection = styled.section`
-	padding: 6rem 2rem;
+	padding: 8rem 4rem 10rem;
 	h2 {
 		margin-bottom: 1em;
 		color: var(--color-neutral-700);
 	}
 	@media (min-width: 576px) {
-		padding: 6rem 4rem;
+		padding: 10rem 4rem 12rem;
 		h2 {
 			font-size: 3rem;
 		}
