6cfa3547a2576ff08453d8121ba60d8e18ebee4f
Aleksander <atkrzanowski@gmail.com>
2023-07-26 11:31:02 +0200

    Fix protected route

--- src/routes/ProtectedLoginFormRoute.jsx
+++ src/routes/ProtectedLoginFormRoute.jsx
@@ -2,9 +2,9 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedLoginFormRoute = ({ children }) => {
-	const { currentUser } = useSelector((store) => store.auth);
+	const { session } = useSelector((store) => store.auth);
 
-	if (currentUser) return <Navigate to='/' />;
+	if (session) return <Navigate to='/' />;
 	return <>{children}</>;
 };
 export default ProtectedLoginFormRoute;
