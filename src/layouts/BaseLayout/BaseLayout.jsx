d0df6822c55a0ea15a39cb1f8d389a437a4941e9
Aleksander <atkrzanowski@gmail.com>
2023-07-28 14:26:22 +0200

    Add loading spinner on page loading in BaseLayout

--- src/layouts/BaseLayout/BaseLayout.jsx
+++ src/layouts/BaseLayout/BaseLayout.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { styled } from 'styled-components';
 import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
 const StyledMain = styled.main`
 	display: block;
 	/* margin-top: var(--height-navbar); */
-	min-height: 80vh;
+	min-height: calc(100vh - var(--height-navbar));
 `;
 
 const BaseLayout = ({
@@ -16,6 +17,9 @@ const BaseLayout = ({
 	socialLinksList,
 	dropdownItemsList,
 }) => {
+	const navigation = useNavigation();
+	const pageLoading = navigation.state === 'loading';
+
 	return (
 		<>
 			<header>
@@ -24,9 +28,7 @@ const BaseLayout = ({
 					dropdownItemsList={dropdownItemsList}
 				/>
 			</header>
-			<StyledMain>
-				<Outlet />
-			</StyledMain>
+			<StyledMain>{pageLoading ? <LoadingSpinner /> : <Outlet />}</StyledMain>
 			<Footer
 				footerLinksList={footerLinksList}
 				socialLinksList={socialLinksList}
