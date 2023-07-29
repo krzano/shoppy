da6d1d299c02e7e2f9ba8fd278687be89d9b403e
Aleksander <atkrzanowski@gmail.com>
2023-07-29 12:57:25 +0200

    Change styled component prop name to $ prefix

--- src/features/products/components/ProductGallery/ProductGallery.jsx
+++ src/features/products/components/ProductGallery/ProductGallery.jsx
@@ -2,7 +2,7 @@ import { styled } from 'styled-components';
 
 const ProductGallery = ({ image, name }) => {
 	return (
-		<StyledProductGallery image={image}>
+		<StyledProductGallery $image={image}>
 			<div className='blur'>
 				<img src={image} alt={name} />
 			</div>
@@ -14,19 +14,17 @@ const StyledProductGallery = styled.div`
 	position: relative;
 	z-index: -10;
 	height: 340px;
-	background-image: url(${(props) => props.image});
+	background-image: url(${({ $image }) => $image});
 	border: 1px solid var(--color-neutral-200);
 	/* border-radius: var(--border-radius-sm); */
 	background-size: cover;
 	background-position: center;
 	.blur {
-		/* padding: 2em; */
 		backdrop-filter: blur(10px);
 		width: 100%;
 		height: 100%;
 	}
 	img {
-		/* box-shadow: var(--shadow-sm); */
 		object-fit: contain;
 		object-position: center;
 		width: 100%;
