695449ed862e735f761f50d1299b867251171715
Aleksander <atkrzanowski@gmail.com>
2023-07-31 17:02:21 +0200

    Add filters functionality in productsSlice

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,14 +1,20 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import supabase from '../../services/supabase/supabase';
 
-const initialFilters = {};
+const initialFilters = {
+	search: '',
+	company: 'all',
+	category: 'all',
+	sortBy: 'name-az',
+};
 
 const initialState = {
 	isLoading: false,
-	//error handling in Products.jsx
+	//TODO: error handling in Products.jsx
 	isError: false,
-	products: [],
+	allProducts: [],
 	filteredProducts: [],
+	filters: { ...initialFilters },
 };
 
 export const getAllProducts = createAsyncThunk(
@@ -25,7 +31,67 @@ export const getAllProducts = createAsyncThunk(
 const productsSlice = createSlice({
 	name: 'products',
 	initialState,
-	reducers: {},
+	reducers: {
+		clearFilters: (state) => {
+			state.filteredProducts = [...state.allProducts];
+			state.filters = { ...initialFilters };
+		},
+		updateFilters: (state, { payload }) => {
+			const { name, value } = payload;
+			state.filters[name] = value;
+		},
+		filterProducts: (state) => {
+			const {
+				allProducts,
+				filters: { search, category, company, sortBy },
+			} = state;
+			let tempProducts = allProducts;
+			if (search) {
+				tempProducts = tempProducts.filter((product) => {
+					return product.name.toLowerCase().includes(search.toLowerCase());
+				});
+			}
+			if (category !== 'all') {
+				tempProducts = tempProducts.filter((product) => {
+					return product.category === category;
+				});
+			}
+			if (company !== 'all') {
+				tempProducts = tempProducts.filter((product) => {
+					return product.company === company;
+				});
+			}
+			if (sortBy === 'name-az') {
+				tempProducts = tempProducts.sort((a, b) => {
+					const nameA = a.name.toLowerCase();
+					const nameB = b.name.toLowerCase();
+					if (nameA < nameB) return -1;
+					if (nameA > nameB) return 1;
+					return 0;
+				});
+			}
+			if (sortBy === 'name-za') {
+				tempProducts = tempProducts.sort((a, b) => {
+					const nameA = a.name.toLowerCase();
+					const nameB = b.name.toLowerCase();
+					if (nameA > nameB) return -1;
+					if (nameA < nameB) return 1;
+					return 0;
+				});
+			}
+			if (sortBy === 'price-lowest') {
+				tempProducts = tempProducts.sort((a, b) => {
+					return a.price - b.price;
+				});
+			}
+			if (sortBy === 'price-highest') {
+				tempProducts = tempProducts.sort((a, b) => {
+					return b.price - a.price;
+				});
+			}
+			state.filteredProducts = tempProducts;
+		},
+	},
 	extraReducers: (builder) => {
 		builder
 			.addCase(getAllProducts.pending, (state) => {
@@ -34,7 +100,8 @@ const productsSlice = createSlice({
 			})
 			.addCase(getAllProducts.fulfilled, (state, { payload }) => {
 				state.isLoading = false;
-				state.products = payload;
+				state.allProducts = payload;
+				state.filteredProducts = payload;
 			})
 			.addCase(getAllProducts.rejected, (state, { payload }) => {
 				state.isLoading = false;
@@ -45,3 +112,6 @@ const productsSlice = createSlice({
 });
 
 export default productsSlice.reducer;
+
+export const { clearFilters, updateFilters, filterProducts } =
+	productsSlice.actions;
