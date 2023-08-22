03708f9f5c840919adbd42e93ed77ad23f27d560
Aleksander <atkrzanowski@gmail.com>
2023-08-22 12:32:11 +0200

    Finish steps container functionality and styling

--- src/features/cart/components/Stepper/Stepper.jsx
+++ src/features/cart/components/Stepper/Stepper.jsx
@@ -1,5 +1,7 @@
 import { styled } from 'styled-components';
 import { createContext, useContext, useState } from 'react';
+import { FaCheck } from 'react-icons/fa6';
+// import { ImCheckmark } from 'react-icons/im';
 
 const StepsContext = createContext();
 export const UseStepsContext = () => useContext(StepsContext);
@@ -17,53 +19,88 @@ const Stepper = ({ steps }) => {
 	};
 
 	const isStepActive = (index) => index === currentStep - 1;
-	const isStepCompleted = (index) => index > currentStep - 1;
+	const isStepCompleted = (index) => index < currentStep - 1;
 
 	return (
-		<StyledStepper>
-			<div className='stepper'>
+		<StyledStepper $numberOfSteps={steps.length}>
+			<div className='steps-container'>
 				{steps.map(({ title }, index) => {
 					return (
 						<div
 							key={title}
-							className={`step ${isStepActive(index) && 'active'} 
-							${isStepCompleted(index) && 'completed'}`}>
-							<p>{index + 1}</p>
-							<p>{title}</p>
+							className={`step ${isStepActive(index) ? 'active' : ''} 
+							${isStepCompleted(index) ? 'completed' : ''}`}>
+							<div className='step-number'>
+								{isStepCompleted(index) ? <FaCheck /> : <p>{index + 1}</p>}
+							</div>
+							<p className='step-title'>{title}</p>
 						</div>
 					);
 				})}
 			</div>
 			<StepsContext.Provider value={{ currentStep, handleNextStep }}>
-				<div>{steps[currentStep - 1].context}</div>
+				<div className='step-content'>{steps[currentStep - 1].context}</div>
 			</StepsContext.Provider>
 		</StyledStepper>
 	);
 };
 
 const StyledStepper = styled.div`
-	.stepper {
+	.steps-container {
 		display: grid;
 		align-items: center;
-		gap: 1rem;
-		row-gap: 3rem;
-		margin-bottom: 3rem;
+		column-gap: 1rem;
+		row-gap: 2rem;
+		margin-bottom: 2rem;
+		padding-left: 2rem;
 		.step {
 			flex-grow: 1;
 			display: flex;
-			flex-direction: column;
-			justify-content: center;
+			flex-direction: row;
+			justify-content: flex-start;
+			column-gap: 2rem;
 			align-items: center;
 			text-transform: capitalize;
-			opacity: 0.5;
+			opacity: 0.4;
+		}
+		.step-number {
+			display: grid;
+			place-items: center;
+			width: 3.6rem;
+			height: 3.6rem;
+			font-size: 2rem;
+			font-weight: bold;
+			border-radius: 50%;
+			border: 3px solid var(--color-neutral-700);
+			background-color: var(--color-neutral-0);
+		}
+		.step-title {
+			font-weight: 500;
 		}
 		.active {
 			opacity: 1;
+			color: var(--color-primary-800);
+			.step-number {
+				border-color: var(--color-primary-800);
+			}
+		}
+		.completed {
+			color: var(--color-primary-800);
+			.step-number {
+				border-color: var(--color-primary-800);
+			}
 		}
 	}
-	@media (min-width: 576px) {
-		.stepper {
+	@media (min-width: ${({ $numberOfSteps }) => $numberOfSteps * 130 + 'px'}) {
+		.steps-container {
 			grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+			padding-left: 0;
+			.step {
+				flex-direction: column;
+			}
+			.step-number {
+				margin-bottom: 1rem;
+			}
 		}
 	}
 `;
