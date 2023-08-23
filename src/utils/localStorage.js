f66279128e78c2dfa97aad1acbd04af16d5faee0
Aleksander <atkrzanowski@gmail.com>
2023-08-23 23:11:41 +0200

    Add shipping info in local storage functions

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,4 +1,5 @@
 const CART_ITEMS = 'CART_ITEMS';
+const SHIPPING_ADDRESS = 'SHIPPING_ADDRESS';
 
 export const updateCartItemsInLocalStorage = (cartItems) => {
 	// localStorage.setItem(CART_ITEMS, JSON.stringify(cartItems || []));
@@ -9,4 +10,16 @@ export const getCartItemsFromLocalStorage = () => {
 	const localStorageCartItems = JSON.parse(localStorage.getItem(CART_ITEMS));
 	// console.log(localStorageCartItems || []);
 	return localStorageCartItems || [];
+};
+
+export const updateShippingAddressInLocalStorage = (shippingAddress) => {
+	localStorage.setItem(SHIPPING_ADDRESS, JSON.stringify(shippingAddress));
+};
+
+export const getShippingAddressFromLocalStorage = () => {
+	const localStorageShippingAddress = JSON.parse(
+		localStorage.getItem(SHIPPING_ADDRESS)
+	);
+	console.log(localStorageShippingAddress);
+	return localStorageShippingAddress;
 };
