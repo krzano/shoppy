c6fee90c7c3263e3baf466c5e3785827c32860c8
Aleksander <atkrzanowski@gmail.com>
2023-10-13 19:56:52 +0200

    Add regex validation for yup address schema

--- src/lib/yup/schemas/addressSchema.js
+++ src/lib/yup/schemas/addressSchema.js
@@ -5,11 +5,20 @@ const addressSchema = yup.object().shape({
 	lastName: yup.string().required('Required'),
 	street: yup.string().required('Required'),
 	city: yup.string().required('Required'),
-	postalCode: yup.string().required('Required'),
+	postalCode: yup
+		.string()
+		.matches(/^[0-9]+[0-9-][0-9]+$/, {
+			message: 'Incorrect postalcode format (only numbers and hyphen)',
+		})
+		.required('Required'),
 	phoneNumber: yup
-		.string('Make sure it is a proper number')
+		.string()
+		// /^\+(?:[0-9]●?){6,14}[0-9]$/ regex for all international phone numbers
+		.matches(/^\+(?:[0-9]●?){6,14}[0-9]$/, {
+			message:
+				'Make sure it is a proper phone number starting with + and your country code',
+		})
 		.required('Required'),
 	// country: yup.string().required('Required'),
 });
-
 export default addressSchema;
