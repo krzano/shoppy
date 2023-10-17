1d26c323cd8745fbf34eff7417ed349d283e4b23
Aleksander <atkrzanowski@gmail.com>
2023-10-17 16:39:32 +0200

    Make section padding smaller

--- src/core/Home/Section/Section.jsx
+++ src/core/Home/Section/Section.jsx
@@ -2,14 +2,14 @@ import { styled } from 'styled-components';
 import StyledContentWrapper from '../../../styles/StyledContentWrapper/StyledContentWrapper';
 
 const StyledSection = styled.section`
-	padding: 6rem 2rem 8rem;
+	padding: 6rem 2rem 6rem;
 	overflow-x: hidden;
 	h2 {
 		margin-bottom: 1em;
 		color: var(--color-neutral-700);
 	}
 	@media (min-width: 576px) {
-		padding: 10rem 4rem 12rem;
+		padding: 8rem 4rem 8rem;
 		h2 {
 			font-size: 3rem;
 		}
