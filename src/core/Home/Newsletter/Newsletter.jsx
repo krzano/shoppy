10f826156688816790111d14e5ea103c8a0e91d8
Aleksander <atkrzanowski@gmail.com>
2023-10-20 16:03:49 +0200

    Change styles for desktop and add loading imitation

--- src/core/Home/Newsletter/Newsletter.jsx
+++ src/core/Home/Newsletter/Newsletter.jsx
@@ -4,8 +4,12 @@ import FormikTextField from '../../../components/FormikTextField/FormikTextField
 import Button from '../../../components/Button/Button';
 import newsletterSchema from '../../../lib/yup/schemas/newsletterSchema';
 import { toast } from 'react-toastify';
+import { useState } from 'react';
 
 const Newsletter = () => {
+	const [isNewsletterSubscriptionLoading, setIsNewsletterSubscriptionLoading] =
+		useState(false);
+
 	return (
 		<StyledNewsletter>
 			<div className='text-container'>
@@ -19,22 +23,32 @@ const Newsletter = () => {
 					email: '',
 				}}
 				validationSchema={newsletterSchema}
-				onSubmit={(values, { resetForm }) => {
-					toast.success('Thank you for subscribing to our newsletter!');
-					resetForm();
+				onSubmit={(_values, { resetForm }) => {
+					setIsNewsletterSubscriptionLoading(true);
+					setTimeout(() => {
+						setIsNewsletterSubscriptionLoading(false);
+						toast.success('Thank you for subscribing to our newsletter!');
+						resetForm();
+					}, 900);
 				}}>
 				<StyledFormikForm>
 					<FormikTextField
 						name='name'
 						showLabel={false}
 						placeholder='Your name*'
+						disabled={isNewsletterSubscriptionLoading}
 					/>
 					<FormikTextField
 						name='email'
 						showLabel={false}
 						placeholder='Your email address*'
+						disabled={isNewsletterSubscriptionLoading}
 					/>
-					<Button $variant='secondary' $size='small' type='submit'>
+					<Button
+						$variant='secondary'
+						$size='small'
+						type='submit'
+						isLoading={isNewsletterSubscriptionLoading}>
 						Subscribe
 					</Button>
 				</StyledFormikForm>
@@ -44,7 +58,8 @@ const Newsletter = () => {
 };
 
 const StyledNewsletter = styled.div`
-	display: grid;
+	display: flex;
+	flex-direction: column;
 	gap: 4rem 2rem;
 	padding: 2rem;
 	h2 {
@@ -54,6 +69,14 @@ const StyledNewsletter = styled.div`
 		font-weight: 500;
 		margin-bottom: 1rem;
 	}
+	@media (min-width: 576px) {
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+	@media (min-width: 768px) {
+		gap: 6rem;
+	}
 `;
 
 const StyledFormikForm = styled(Form)`
@@ -62,7 +85,7 @@ const StyledFormikForm = styled(Form)`
 	gap: 2rem;
 	padding: 3rem 2rem;
 	background-color: var(--color-primary-50);
-	border: 2px solid var(--color-primary-200);
+	border: 1px solid var(--color-primary-200);
 	border-radius: var(--border-radius-lg);
 	box-shadow: var(--shadow-md);
 `;
