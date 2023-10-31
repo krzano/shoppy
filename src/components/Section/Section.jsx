98edc0c4029c53a2106d937c7817dcf29299d678
Aleksander <atkrzanowski@gmail.com>
2023-10-31 22:27:00 +0100

    Fix title styling using styled components

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -6,12 +6,19 @@ const Section = ({ title, children, $textColorVariant = 'dark' }) => {
 	return (
 		<StyledSection $textColorVariant={$textColorVariant}>
 			<StyledContentWrapper>
-				{title && <h2 className='title'>{title}</h2>}
+				{title && <StyledTitle>{title}</StyledTitle>}
 				{children}
 			</StyledContentWrapper>
 		</StyledSection>
 	);
 };
+
+const StyledTitle = styled.h2`
+	margin-bottom: 1.6em;
+	@media (min-width: 576px) {
+		font-size: 3rem;
+	}
+`;
 
 const StyledSection = styled.section`
 	padding: 6rem 2rem 6rem;
@@ -29,14 +36,8 @@ const StyledSection = styled.section`
 			`;
 		}
 	}}
-	.title {
-		margin-bottom: 1.6em;
-	}
 	@media (min-width: 576px) {
 		padding: 6rem 4rem 8rem;
-		.title {
-			font-size: 3rem;
-		}
 	}
 `;
 
